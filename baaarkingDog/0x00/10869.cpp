20bd6e834ce8b3fd733ca7753c7bfc78d9d03862
hoony0608 <rhdqndyd0608@gmail.com>
2024-02-20 22:27:35 +0900

    Create 10869.cpp

---
+++ baaarkingDog/0x00/10869.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b;
+
+int main() {
+	
+	cin >> a >> b;
+	
+	cout << a + b << "\n" << a - b << "\n";
+	cout << a * b << "\n" << a / b << "\n";
+	cout << a % b << "\n";
+	
+	return 0;
+}
