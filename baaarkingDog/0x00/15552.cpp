5ee44c05ddf09e4718cc9bdce758da3673fcd888
hoony0608 <rhdqndyd0608@gmail.com>
2024-02-21 16:45:32 +0900

    Create 15552.cpp

---
+++ baaarkingDog/0x00/15552.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b, n;
+
+int main() {
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n;
+	while(n--) {
+		cin >> a >> b;
+		cout << a + b << "\n";
+	}
+	 
+	return 0;
+}
