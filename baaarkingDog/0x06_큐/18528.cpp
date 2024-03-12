6b32db626aee5633d9ef8b02cb582978dda1dc4c
hoony0608 <rhdqndyd0608@gmail.com>
2024-03-12 23:03:37 +0900

    Create 18528.cpp

---
+++ baaarkingDog/0x06_큐/18528.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int _max = 2000004;
+
+int n, dat[_max], head = 0, tail = 0, x;
+string cmd;
+
+void push(int x) {
+	dat[tail++] = x; 
+}
+
+void pop() {
+	head++;
+}
+
+bool isEmpty() {
+	return (tail == head) ? 1 : 0;
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n;
+	
+	while(n--) {
+		cin >> cmd;
+		
+		if(cmd == "push") {
+			cin >> x;
+			push(x);
+		} else if (cmd == "pop") {
+			if(isEmpty()) cout << -1 << "\n";
+			else {
+				cout << dat[head] << "\n"; 
+				pop();
+			}
+		} else if (cmd == "size") {
+			cout << tail - head << "\n";
+		} else if (cmd == "empty") {
+			cout << isEmpty() << "\n";
+		} else if (cmd == "front") {
+			if(isEmpty()) cout << -1 << "\n";
+			else cout << dat[head] << "\n";
+		} else if (cmd == "back") {
+			if(isEmpty()) cout << -1 << "\n";
+			else cout << dat[tail - 1] << "\n"; 
+		}
+		
+	}
+	
+	return 0;
+}
